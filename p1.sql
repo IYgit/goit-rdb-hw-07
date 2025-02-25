979415892030889cafed1a5a8bdce84811b5df6d
i.yanchyk <i.yanchyk@steel.mil.ua>
2025-02-25 06:14:08 +0200

    done

---
+++ p1.sql
@@ -0,0 +1,9 @@
+use mydb;
+
+SELECT 
+    id, 
+    date, 
+    EXTRACT(YEAR FROM date) AS year, 
+    EXTRACT(MONTH FROM date) AS month, 
+    EXTRACT(DAY FROM date) AS day
+FROM orders;
---
+++ p2.sql
@@ -0,0 +1,7 @@
+use mydb;
+
+SELECT 
+    id, 
+    date, 
+    DATE_ADD(date, INTERVAL 1 DAY) AS new_date
+FROM orders;
